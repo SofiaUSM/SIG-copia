4ee901c9a5b6ac14a8b148360193e0d99145800f
H-Emanuel <emanuelvperez2000@gmail.com>
2024-01-17 11:18:51 -0300

    Limpiar la caché

--- formulario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'solicitud'
-
-urlpatterns = [
-    path('',views.crear_protocolo),
-    path('vista_previa/<int:id>', views.vista_previa, name="vista_previa"),
-]

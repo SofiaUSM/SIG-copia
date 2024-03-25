780aa92d2edb162638afa52e27987bd54a539c06
H-Emanuel <emanuelvperez2000@gmail.com>
2024-03-25 11:42:28 -0300

    cambios_pequeños

--- formulario/views.py
+++ formulario/views.py
@@ -27,9 +27,10 @@ import string
 
 
 def generar_codigo():
-    caracteres = string.ascii_letters + string.digits
+    caracteres = '0123456789'  # Solo dígitos
     codigo = ''.join(random.choices(caracteres, k=10))
     return codigo
+
 
 def codigo_es_duplicado(codigo):
     return ProtocoloSolicitud.objects.filter(codigo=codigo).exists()

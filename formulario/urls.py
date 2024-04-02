62c2fd739e787e04215acb5fe522b8430e5743e6
H-Emanuel <emanuelvperez2000@gmail.com>
2024-04-02 09:30:15 -0300

    DESCARGAR_PDF

--- formulario/urls.py
+++ formulario/urls.py
@@ -5,5 +5,7 @@ app_name = 'solicitud'
 
 urlpatterns = [
     path('',views.crear_protocolo),
-    path('vista_previa/<int:id>', views.vista_previa, name="vista_previa"),
+    path('vista_previa/<int:id>', views.vista_previa, name="vista_previa"),     
+    path('descargar_pdf/<int:id>', views.descargar_pdf, name="descargar_pdf"),
+
 ]

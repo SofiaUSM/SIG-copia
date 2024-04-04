5d28994adf5f3cf98b043bde68c5865c11a1a7f7
H-Emanuel <emanuelvperez2000@gmail.com>
2024-04-04 10:34:47 -0300

    cambio

--- web_control_usuario/settings.py
+++ web_control_usuario/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sz308p48$%9_v+&$*=d)gd2dak5*n-2-zr5989(c#d(r+9)h55'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.12.80','departamentosig.munivalpo.cl','localhost',"*"]
 

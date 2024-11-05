8d6e47786c4f6753f08e89e6e723764588200195
H-Emanuel <emanuelvperez2000@gmail.com>
2024-11-05 13:26:44 -0300

    cambio_archivo

--- core/views.py
+++ core/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import redirect
 from .models import *
 from formulario.views import *
 from urllib.parse import unquote
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer
+from datetime import datetime
+
 
 def inicio(request):
     return render(request,'core/iniciar.html')
@@ -24,34 +28,38 @@ def arcgisregister(request):
     departamento_seleccionado = request.COOKIES.get('departamento')
 
     if departamento_seleccionado:
-        # Guarda el UserActivity con la información del departamento seleccionado
+
+
         user_activity = UserActivity(page='experience.arcgis.com', departamento=departamento_seleccionado)
         user_activity.save()
-
-        # Crear una nueva entrada en el modelo UserActivity con el departamento seleccionado
-        UserActivity.objects.create(page="https://experience.arcgis.com/", departamento=departamento_seleccionado)
+        Registro_arcgis(departamento_seleccionado,'experience.arcgis.com')  
 
     # Redirigir al usuario al enlace de ArcGIS
     return redirect('https://experience.arcgis.com/experience/6a6b0cbfb2094d10ba11b439c8060a8d/?return_url=/some_view/')
+
 def geoportalVisita(request):
 
     departamento_seleccionado = request.COOKIES.get('departamento')
 
     if departamento_seleccionado:
-        # Guarda el UserActivity con la información del departamento seleccionado
+
         user_activity = UserActivity(page='Geoportal-sig-munivalpo.hub.arcgis.com', departamento=departamento_seleccionado)
         user_activity.save()   
+        Registro_arcgis(departamento_seleccionado,'Gestion SIG')  
 
     return redirect('https://geoportal-sig-munivalpo.hub.arcgis.com/')
 
 def gestionsig(request):
-
     departamento_seleccionado = request.COOKIES.get('departamento')
 
+
+
+
+
     if departamento_seleccionado:
-        # Guarda el UserActivity con la información del departamento seleccionado
         user_activity = UserActivity(page='Gestion SIG', departamento=departamento_seleccionado)
         user_activity.save()   
+        Registro_arcgis(departamento_seleccionado,'Gestion SIG')  
 
     return redirect('https://www.arcgis.com/apps/dashboards/b35864f66368465fa11b6c40b1321688')
 
@@ -59,10 +67,18 @@ def SSregistro(request):
     departamento_seleccionado = request.COOKIES.get('departamento')
 
     if departamento_seleccionado:
-        # Guarda el UserActivity con la información del departamento seleccionado
+
+
+
+
+
+
         user_activity = UserActivity(page='Pagina Solicitudes', departamento=departamento_seleccionado)
-        user_activity.save()   
+        user_activity.save()
+        Registro_arcgis(departamento_seleccionado,'Pagina Solicitudes')  
     return redirect('/solicitud/')
+
+
 def buscar_protocolo(request):
   if request.method == "POST":
     codigo = request.POST.get("codigo")
@@ -72,11 +88,105 @@ def buscar_protocolo(request):
       estado_protocolo = solicitud.estado
     except ProtocoloSolicitud.DoesNotExist:
       estado_protocolo = "No existe el código de protocolo"
-    # Devolver el estado del protocolo o el mensaje de error como una respuesta JSON
     data = {
       "estado": estado_protocolo
     }
     return JsonResponse(data)
 
+def Registro_arcgis(depa, pagina):
+    try:
+        # Obtener la fecha y hora actual
+        now = datetime.now()
 
+        # Diccionario para los meses con el formato personalizado
+        meses = {
+            1: "1-ENERO",
+            2: "2-FEBRERO",
+            3: "3-MARZO",
+            4: "4-ABRIL",
+            5: "5-MAYO",
+            6: "6-JUNIO",
+            7: "7-JULIO",
+            8: "8-AGOSTO",
+            9: "9.1-SEPTIEMBRE",
+            10: "9.2-OCTUBRE",
+            11: "9.3-NOVIEMBRE",
+            12: "9.4-DICIEMBRE"
+        }
+
+        # Obtener el año en número
+        año = now.year
+
+        # Obtener el mes en número
+        mes_numero = now.month
+
+        # Obtener el mes en formato personalizado
+        mes_formato = meses.get(mes_numero, "Mes no válido")
+        formatted_time = now.strftime("%m-%d-%Y-%H:%M")
+        sigla = obtener_sigla(depa)
+
+        # Conectar a ArcGIS
+        gis = GIS("https://www.arcgis.com", "jimmi.gomez_munivalpo", "Jimgomez8718")
+
+        # URL de la capa
+        layer_url = "https://services8.arcgis.com/5BaAHTQ4nRVz57H5/arcgis/rest/services/Registro_de_Usuarios/FeatureServer/0"
+        feature_layer = FeatureLayer(layer_url)
+
+        # Obtener la última Id en la capa
+        query_result = feature_layer.query(where="1=1", order_by_fields="Id DESC", out_fields="Id", return_geometry=False)
+        last_Id = query_result.features[0].attributes['Id'] if query_result.features else 0
+
+        # Calcular la nueva Id
+        new_Id = last_Id + 1
+
+        # Definir los atributos de los nuevos datos
+        new_feature = {
+            "attributes": {
+                "Id": new_Id,           
+                "Departemen": depa,            
+                "Fecha": formatted_time, 
+                "MES": mes_formato, 
+                "Pagina": pagina,           
+                "Direc_Dept": depa,            
+                "Sigla": sigla, 
+                "AÑO": año,           
+            }
+        }
+
+        # Crear una lista con la nueva característica
+        features_to_add = [new_feature]
+
+        # Agregar las nuevas características a la capa
+        response = feature_layer.edit_features(adds=features_to_add)
+        
+        # Verificar si la respuesta fue exitosa
+        if 'addResults' in response and response['addResults'][0]['success']:
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        return False
+
+def obtener_sigla(departamento):
+
+    departamentos = {
+    "Alcaldia": "ALCALDIA",
+    "Dirección de Administración y Finanzas": "DAF",
+    "Dirección de Asesoría Jurídica": "DAJ",
+    "Dirección desarrollo Económico y Cooperación Internacional": "DES_ECONOMICO",
+    "Dirección de Desarrollo Comunitario": "DIDECO",
+    "Dirección de Género, Mujeres y Diversidades": "GENERO",
+    "Dirección de Medioambiente": "MEDIO AMBIENTE",
+    "Dirección de Obras Municipales": "OBRAS MUNICIPALES",
+    "Dirección de Operaciones": "OPERACIONES",
+    "sin direccion": "Periodo Marcha Blanca",
+    "SECPLA": "SECPLA",
+    "Dirección de Seguridad Ciudadana": "SEGURIDAD",
+    "Dirección de Tránsito y Transporte público": "TRANSITO",
+    "Dirección de Vivienda, Barrio y Territorio": "VIVIENDA"
+    }
+
+    return departamentos.get(departamento, "Departamento no encontrado")
 
